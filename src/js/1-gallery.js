5d279daa1f3eac6c7fdcb56fa5cfe72b07f785ce
IvanBuzin <ua.buzin@gmail.com>
2023-12-30 00:17:30 +0200

    gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -92,3 +92,15 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
+lightbox.on('shown.simplelightbox', (e) => {
+ 
+  setTimeout(() => {
+    const { alt } = e.target;
+    const captionElement = e.target.getElementsByClassName('sl-image-caption')[0];
+    
+    if (captionElement) {
+      captionElement.textContent = alt;
+    }
+  }, 250);
+});
