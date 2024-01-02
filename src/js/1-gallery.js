61cd8363d402309f14d37846e407f5df24869ac3
IvanBuzin <ua.buzin@gmail.com>
2024-01-03 01:34:48 +0200

    ====

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -87,4 +87,4 @@ gallery.innerHTML = images.reduce(
   ""
 );
 
-let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
+const gallery = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
